64576b1b5708b76c67e6b8856db1d5bcd368c74c
Muna Adan <adan0027@algonquinlive.com>
2022-03-07 18:49:37 -0500

    Day 2 Excersise Part 1

---
+++ Day2Excersises/challenge1.js
@@ -0,0 +1,36 @@
+let challenge = ' 30 Days of JavaScript '
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substr(3,21))
+console.log(challenge.substr(0,3))
+console.log(challenge.includes('Script'))
+console.log(challenge.split())
+console.log(challenge.split(' '))
+console.log(challenge.replace('JavaScript', 'Python'))
+console.log(challenge.charAt(15))
+console.log(challenge.charCodeAt('J'))
+console.log(challenge.indexOf('a'))
+console.log(challenge.lastIndexOf('a'))
+console.log(challenge.trim('30 Days of JavaScript'))
+console.log(challenge.startsWith(' 30 Days of JavaScript '))
+console.log(challenge.endsWith(' 30 Days of JavaScript '))
+console.log(challenge.match('a'))
+console.log(challenge.repeat(2))
+
+
+let string = '30'
+console.log(string.concat('Days', 'Of', 'JavaScript'))
+
+let socialPlatforms = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+
+console.log(socialPlatforms.split(','))
+
+let sentence = 'You cannot end a sentence with because because because is a conjunction'
+console.log(sentence)
+console.log(sentence.indexOf('because'))
+console.log(sentence.lastIndexOf('because'))
+console.log(sentence.search('because'))
+
+
---
+++ Day2Excersises/challenge2.js
@@ -0,0 +1,20 @@
+let quote = 'There is no exercise better for the heart than reaching down and lifting people up.'
+console.log(quote)
+
+let saying = 'Love is not patronizing and charity is not about pity, it is about love. Charity and love are the same -- with charity you give love, so do not just give money but reach out your hand instead.'
+
+console.log(saying)
+
+
+let num = '9.8'
+let numFloat = parseFloat(10)
+console.log(numFloat)
+
+let words = 'python and jargon'
+console.log(words.includes('on'))
+
+const randNum = Math.floor(Math.random() * 101)
+console.log(randNum)
+
+const rndInt = Math.floor(Math.random() * 50) +101
+console.log(rndInt)
